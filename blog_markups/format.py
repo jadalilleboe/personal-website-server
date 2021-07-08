2ec6cadfe2d0cbdd61cc7f68c4c8af5fc5a125f4
Jada Lilleboe <lilleboejada@gmail.com>
2021-07-08 13:31:16 -0500

    adds formatting python helper file

---
+++ blog_markups/format.py
@@ -0,0 +1,15 @@
+def format_markup(fname):
+    with open(f'blog_markups\{fname}', 'r') as f:
+        contents = f.read()
+    format = ''
+    for char in contents:
+        if char == '\n':
+            pass
+        elif char == '"':
+            format += '\\"'
+        else:
+            format += char
+    print(format)
+
+if __name__ == '__main__':
+    format_markup('learnjs.html')
